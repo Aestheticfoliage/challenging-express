0f289d222528a4c4ce93cf4893e7eb4717084651
dewey preast <webwalkin@me.com>
2022-05-22 10:59:52 -0400

    added router and checking push and get

---
+++ routes/apiRoutes.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+
+router.get("/notes", (req, res) => {
+  res.json("get!");
+});
+router.post("/notes", (req, res) => {
+  res.json("post!");
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
-
+const router = require("express").Router();
+const htmlRoutes = require("./routes/htmlRoutes");
+const apiRoutes = require("./routes/apiRoutes");
 // initialize the app and create a port
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -7,6 +9,13 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+// api routes
+
+app.use("/api", apiRoutes);
+
+// html routes
+app.use("/", htmlRoutes);
 
 // this is starting the server on the port
 app.listen(PORT, () => console.log("Listening on PORT: ${PORT}"));
