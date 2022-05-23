8e1a82357694549590d66de8850c0f207859e046
dewey preast <webwalkin@me.com>
2022-05-22 20:27:50 -0400

    completing most of tasks, working on making sure we are functional, working as far as insomnia up to this point

---
+++ db/store.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+
+// This package will be used to generate our unique id
+const uuidv1 = require("uuid/v1");
+const util = require("util");
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+class Store {
+  getNotes() {
+    return readFileAsync("db/db.json", "utf-8").then((notes) => {
+      return JSON.parse(notes);
+      //         try {
+      //                 parsedNotes = [].concat(JSON.parse(notes))
+      //         } catch (err){
+      //                 parsedNotes = []
+      //         }
+    });
+  }
+  addNote(note) {
+    const { title, text } = note;
+
+    if (!title || !text) {
+      throw new Error("Title and text are required");
+    }
+    const newNote = { title, text, id: uuidv1() };
+    this.getNotes().then((notes) => {
+      console.log(notes);
+    });
+  }
+}
+module.exports = new Store();
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,21 @@
 const router = require("express").Router();
+const { v4: uuidv4 } = require("uuid");
+const {
+  readFromFile,
+  readAndAppend,
+  writeToFile,
+} = require("../helpers/fsUtils");
 
 router.get("/notes", (req, res) => {
+  readFromFile("./db/db.json").then(() => res.json(JSON.parse(data)));
   res.json("get!");
 });
 router.post("/notes", (req, res) => {
-  res.json("post!");
+  const newNote = req.body;
+  newNote.id = uuidv4();
+
+  readAndAppend(newNote, "./db/db.json");
+  res.json(newNote);
 });
 
 module.exports = router;
